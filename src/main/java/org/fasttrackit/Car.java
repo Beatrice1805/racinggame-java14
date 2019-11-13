06d578a6588649a7a788411399d8fdc6e31d1a76
Beatrice <oprea.beatrice@yahoo.com>
2019-11-13 21:31:04 +0200

    commented out NullPointerException example

--- src/main/java/org/fasttrackit/Car.java
+++ src/main/java/org/fasttrackit/Car.java
@@ -1,14 +1,7 @@
 package org.fasttrackit;
+// inheritance or "is-a relationship"
+public class Car extends Vehicle {
 
-public class Car {
 
-    // instance variables
-    String name;
-    String color;
-    double mileage;
-    double maxSpeed;
-    double fuelLevel;
-    double traveledDistance;
-    boolean running;
     int doorCount;
 }
